85bbbe86179632fc6acf4c719cd50c9147854098
uno <rubenlladoc@gmail.com>
2019-08-27 13:04:12 +0800

    working with db

---
+++ src/registration_class.java
@@ -0,0 +1,45 @@
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author uxer
+ */
+public class registration_class {
+    
+    public void register(String username, String password, String firstname, String lastname){
+    //String sql = "insert into users values(null,'"+username+"',md5('"+password+"'),'"+lastname+"','"+firstname+"',0)";    
+    //System.out.println(sql);
+    conn con = new conn();
+    try{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = (Connection) DriverManager.getConnection(con.url, con.username, con.password);
+        String sql = "insert into users values(null,?,md5(?),?,?,0)";
+        PreparedStatement pstmt = (PreparedStatement) conn.prepareStatement(sql);
+        
+        pstmt.setString(1, username);
+        pstmt.setString(2, password);
+        pstmt.setString(3, lastname);
+        pstmt.setString(4, firstname);
+        
+        int x = pstmt.executeUpdate();
+        System.out.println(x);
+    }   catch (ClassNotFoundException ex) {
+            Logger.getLogger(registration_class.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(registration_class.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
