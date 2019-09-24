705238040c7e4959ce033fa7169b6b415e5c3b2f
uno <unolladoc@gmail.com>
2019-09-24 09:02:42 +0800

    Added "Add PRoduct"

--- src/product.java
+++ src/product.java
@@ -1,3 +1,11 @@
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,10 +17,32 @@
  * @author uxer
  */
 public class product {
-   
-    public void addProduct(String product_name, int quantity, Object price){
-        String sql = "insert into products values(null,'"+product_name+"',"+quantity+","+price+")";
-        System.out.println(sql);
+    conn con = new conn();
+    public int addProduct(String product_name, int quantity, Object price){
+        int r = 0;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = (Connection) DriverManager.getConnection(con.url,con.username,con.password);
+            
+            String sql = "insert into products values(null,?,?,?);";
+            PreparedStatement pstmt = (PreparedStatement) conn.prepareStatement(sql);
+            
+            String np = price.toString();
+            float newprice = Float.parseFloat(np);
+                    
+            pstmt.setString(1, product_name);
+            pstmt.setInt(2, quantity);
+            pstmt.setFloat(3, newprice);
+            
+            r = pstmt.executeUpdate();
+            //System.out.println(pstmt);
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(product.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(product.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return r;
     }
     
 }
